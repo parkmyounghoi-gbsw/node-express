1a4c19890cebe28acca0909c5e1e4bf1ca0ffe23
parkmyounghoi-gbsw <parkmyounghoi@sc.gyo6.net>
2026-06-01 11:10:52 +0900

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.send("제발 클라우드 타입 회원가입 하시고 체크카드 등록해오세요.");
 });
 
+const users = [{name:"홍길동"}]
+
+router.get('/users', function(req, res, next) {
+  res.json(users);
+});
+
 module.exports = router;
